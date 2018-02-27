75b4498e92f46542a374ab2e4a8e41708a43126f
mudassir <mudassir7446@gmail.com>
2018-02-27 23:36:19 +0530

    Tutorial practice for C language

---
+++ c_tutorial/var_x.c
@@ -0,0 +1,12 @@
+/**
+ * * This program shows the output when the variable is only declared and prited
+ *
+ * Variable x is initialized to 0
+ */
+#include <stdio.h>
+int main(){
+int x;
+printf("Value of x : %d",x);
+getchar();
+return 0;
+}
